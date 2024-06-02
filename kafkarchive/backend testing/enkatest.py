a7304b5415cbbdba1cdfc2d749872676c0eb0bbf
Kiryuuin <doraemonstichisnoon@gmail.com>
2024-06-03 00:26:17 +0700

    so close yet so far calculation almost done. only traces left

--- kafkarchive/backend testing/enkatest.py
+++ kafkarchive/backend testing/enkatest.py
@@ -4,6 +4,7 @@ import json
 
 userid = input("Enter your UID: ")
 
+
 async def main() -> None:
     async with enka.HSRClient("en", headers={"Kiryuuin":"testing for a school project website"}) as client:
         fetched = await client.fetch_showcase(userid)
@@ -15,6 +16,9 @@ async def main() -> None:
 
             hp_percent = []
             flat_hp = []
+
+            atk_percent = []
+            flat_atk = []
 
             spd_percent = []
             flat_spd = []
@@ -39,12 +43,17 @@ async def main() -> None:
                 fetchedlist.update({relictype[relic.type-1] + " " + mainstat.name: mainstat.formatted_value})
                 if mainstat.name == "HP" and mainstat.is_percentage==True:
                     hp_percent.append(float(mainstat.formatted_value.replace("%","")))
+                elif mainstat.name == "ATK" and mainstat.is_percentage==True:
+                    atk_percent.append(float(mainstat.formatted_value.replace("%","")))
                 elif mainstat.name == "SPD" and mainstat.is_percentage==True:
                     spd_percent.append(float(mainstat.formatted_value.replace("%","")))
                 if mainstat.name == "HP" and mainstat.is_percentage==False:
                     flat_hp.append(int(mainstat.formatted_value))
+                elif mainstat.name == "ATK" and mainstat.is_percentage==False:
+                    flat_atk.append(int(mainstat.formatted_value))
                 elif mainstat.name == "SPD" and mainstat.is_percentage==False:
                     flat_spd.append(float(mainstat.formatted_value))
+                
                 
                 substatnum = 0
                 for substats in relic.sub_stats:
@@ -52,27 +61,42 @@ async def main() -> None:
                     fetchedlist.update({substats.name + " " + relictype[relic.type-1] + " "+ str(substatnum): substats.formatted_value})
                     if substats.name == "HP" and substats.is_percentage==True:
                         hp_percent.append(float(substats.formatted_value.replace("%","")))
+                    elif substats.name == "ATK" and substats.is_percentage==True:
+                        atk_percent.append(float(substats.formatted_value.replace("%","")))
                     elif substats.name == "SPD" and substats.is_percentage==True:
                         spd_percent.append(float(substats.formatted_value.replace("%","")))
                     if substats.name == "HP" and substats.is_percentage==False:
                         flat_hp.append(int(substats.formatted_value))
+                    elif substats.name == "ATK" and substats.is_percentage==False:
+                        flat_atk.append(int(substats.formatted_value))
                     elif substats.name == "SPD" and substats.is_percentage==False:
                         flat_spd.append(float(substats.formatted_value))
                 #working on it, im having a brain aneurysm
-
-                #for trace in character.traces:
-                #    print(json.dumps(character.traces, indent=4, sort_keys=False))
+            #print(type(character.traces))
+            
+            #print("\n".join([str(i) for i in character.traces]))
 
             summed_hp_percent = sum(hp_percent)
             summed_flat_hp = int(sum(flat_hp))
+
+            summed_atk_percent = sum(atk_percent)
+            summed_flat_atk = int(sum(flat_atk))
 
             summed_spd_percent = sum(spd_percent)
             summed_flat_spd = float(sum(flat_spd))
 
             #please fetch traces data
-            calculated_hp = (int(fetchedlist["Base HP"]) + int(fetchedlist["LCBase HP"]))*(1+(summed_hp_percent*0.01)) + summed_flat_hp
-            print(calculated_hp)
+            print(character.name)
+            print(flat_hp)
+
+            #(BASE HP)*
+
+            calculated_hp = (int(fetchedlist["Base HP"]))*(1+(summed_hp_percent/100)) + summed_flat_hp
+            print(f"HP: {calculated_hp}")
+            calculated_atk = (int(fetchedlist["Base ATK"]))*(1+(summed_atk_percent/100)) + summed_flat_atk
+            print(f"ATK: {calculated_atk}")
             calculated_spd = (float(fetchedlist["SPD"])) * (1+summed_spd_percent)+summed_flat_spd
-            print(character.name+" "+str(calculated_spd))
+            print(f"SPD: {calculated_spd}")
+            
             #print(json.dumps(fetchedlist, indent=4, sort_keys=False))
 asyncio.run(main())
