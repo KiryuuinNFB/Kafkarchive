a4eebc83a22a3dc9b9dc0a77b388e5730dd6b84c
Kiryuuin <doraemonstichisnoon@gmail.com>
2024-06-03 20:57:10 +0700

    implementing traces in calculation

--- kafkarchive/backend testing/enkatest.py
+++ kafkarchive/backend testing/enkatest.py
@@ -5,15 +5,16 @@ import yatta
 
 userid = input("Enter your UID: ")
 
+charaid = 1005
 
 async def main() -> None:
     async with enka.HSRClient("en", headers={"Kiryuuin":"testing for a school project website"}) as client:
         fetched = await client.fetch_showcase(userid)
-        
         print("Name ",fetched.player.nickname)
         print("Level ",fetched.player.level)
 
         for character in fetched.characters:
+
             fetchedlist = {}
 
             hp_percent = []
@@ -28,8 +29,34 @@ async def main() -> None:
             spd_percent = []
             flat_spd = []
 
+            hptrace = {}
+            atktrace = {}
+            deftrace = {}
+            spdtrace = {}
+
+            traceval = {}
+
+            yattaapi = yatta.YattaAPI(headers={"Kiryuuin":"testing for a school project website"})
+            await yattaapi.start()
+            fromyatta = await yattaapi.fetch_character_detail(character.id)
+            traceval = {}
+            for yattatrace in fromyatta.traces.sub_skills:
+                #print(yattatrace.name)
+                #print(yattatrace.id)
+                for stats in yattatrace.status_list:
+                    traceval.update({yattatrace.id:stats.value})
+                    if "HP" in yattatrace.name:
+                        hptrace.update({yattatrace.id:float(traceval[yattatrace.id])})
+                    elif "ATK" in yattatrace.name:
+                        atktrace.update({yattatrace.id:float(traceval[yattatrace.id])})
+                    elif "DEF" in yattatrace.name:
+                        deftrace.update({yattatrace.id:float(traceval[yattatrace.id])})
+                    elif "SPD" in yattatrace.name:
+                        spdtrace.update({yattatrace.id:float(traceval[yattatrace.id])})
+            
+            await yattaapi.close()
+                      
             fetchedlist.update({"Character": character.name })
-            print(character.id)
             for stat in character.stats:
                 fetchedlist.update({stat.name: stat.formatted_value })
             lc = character.light_cone
@@ -87,16 +114,47 @@ async def main() -> None:
                         flat_spd.append(float(substats.formatted_value))
             
                 #working on it, im having a brain aneurysm
-            """
-            tracetype = ["STAT","SKILL","TALENT"]
-            for traceid in character.traces:
-                print(character.name)
-                print(tracetype[traceid.type-1])
-                print(traceid.id)
-            """
-
             
-        
+            tracetype = ["STAT","SKILL","TALENT"]
+            charatraces = {}
+
+            chara_hp_trace = []
+            chara_atk_trace = []
+            chara_def_trace = []
+            chara_spd_trace = []
+
+            for traceid in character.traces:
+                if traceid.type-1 == 0:
+                    charatraces.update({tracetype[traceid.type-1]: traceid.id})
+
+                    if charatraces["traceid.id"] in hptrace:
+                        try:
+                            chara_hp_trace.append(float(hptrace))
+                        except KeyError:
+                            raise
+                    elif charatraces["traceid.id"] in atktrace:
+                        try:
+                            chara_atk_trace.append(float(atktrace))
+                        except KeyError:
+                            raise
+                    elif charatraces["traceid.id"] in deftrace:
+                        try:
+                            chara_def_trace.append(float(deftrace))
+                        except KeyError:
+                            raise
+                    elif charatraces["traceid.id"] in spdtrace:
+                        try:
+                            chara_spd_trace.append(float(spdtrace))
+                        except KeyError:
+                            raise
+                    #print(json.dumps(charatraces, indent=4, sort_keys=False))
+            print(chara_hp_trace)
+            print(chara_atk_trace)
+            print(chara_def_trace)
+            print(chara_spd_trace)
+            
+            """
+            
             summed_hp_percent = sum(hp_percent)
             summed_flat_hp = int(sum(flat_hp))
 
@@ -135,6 +193,6 @@ async def main() -> None:
             calculated_spd = (float(fetchedlist["SPD"])) * (1+summed_spd_percent/100) + summed_flat_spd
             print(f"Calculated SPD: {calculated_spd}")
             print(json.dumps(fetchedlist, indent=4, sort_keys=False))
-            
+            """
 
 asyncio.run(main())
