717d041ef1d6fdf0402422f22eec7e14ccc47587
Kiryuuin <doraemonstichisnoon@gmail.com>
2024-07-04 20:30:56 +0700

    starting to use sveltekit

---
+++ kafkarchive/front/Kafkarchive/src/routes/about/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	let sitetitle = "About | Kafkarchive";
+</script>
+
+<svelte:head>
+	<title>{sitetitle}</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<body>
+	<div class="content">
+		<h1 class="topic">About</h1>
+		<h2 class="subtopic">About Kafkarchive</h2>
+		<div class=walloftext>
+			<p>Kafkarchive is a Honkai: Star Rail database website where players can upload their character builds with tags that best describe them.
+				So that other players can look up builds that might fit their playstyles. It is also a site for calculating hypothetical builds. You can import your builds by putting in your UID, 
+				Or you can create your build manually.
+				This site was created for a school project, So don't expect super accurate information or optimized system. 
+			</p>
+		</div>
+		<h2 class="subtopic">About the developer</h2>
+    <div class="walloftext">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum quam voluptatibus rerum a? Voluptas quaerat nisi minus nihil, non magni odit harum exercitationem? Fuga omnis obcaecati, veniam ex est atque laudantium distinctio excepturi corporis. Eaque, voluptatum nesciunt! Reprehenderit, quisquam. Suscipit sit fugit iste ducimus tenetur numquam ad expedita corporis architecto aut eius aspernatur voluptatum alias magni repellat a dolor sequi explicabo, voluptates quasi sed odit temporibus quod obcaecati. Animi, cumque. Doloribus, cum suscipit. Obcaecati vitae deleniti, qui quam aliquid eos et eveniet aspernatur necessitatibus repudiandae sint incidunt, reiciendis saepe omnis voluptates quaerat expedita molestiae blanditiis fugiat cupiditate quasi ipsum dolorem veniam! Minus quis reprehenderit, tempora debitis consequuntur amet ab facere quibusdam. Sunt obcaecati voluptate veritatis sit culpa sapiente. Minus porro dolore itaque possimus ea. Odit, tempore voluptate eos vel dolorum reiciendis veniam, nobis velit dolore explicabo ipsam. Facere esse quo vero rerum fugit aliquid magnam quae quod odit dolorum tenetur eos nulla ipsam accusantium, error rem, magni explicabo nemo! Sit, similique laudantium ipsum at quo rerum ea totam quibusdam. Architecto incidunt, nesciunt dolorem natus neque doloribus deserunt. Doloribus esse facere veniam, nostrum labore numquam asperiores rem tenetur totam tempore expedita, quisquam, magnam voluptatum aut illo! Fugiat facilis cumque doloribus.
+		</p>
+    </div>
+
+	</div>
+	<div class="emptyspace">
+
+	</div>
+</body>
+
+<style>
+	
+
+</style>
